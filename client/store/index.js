0861c8feeb385e6393d41b81425f9f65c81308be
Stephen Parker <sparkerr.96@gmail.com>
2023-06-26 16:53:27 -0700

    Fresh start

---
+++ client/store/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
+
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(createLogger()),
+});
+
+export default store;
